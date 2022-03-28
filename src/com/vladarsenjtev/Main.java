05adcd188ce9f02c92a3e4305d50d418ff3cf84b
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-03-28 22:50:07 +0300

    Task_2

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,7 +1,18 @@
 package com.vladarsenjtev;
 
+
 public class Main {
 
     public static void main(String[] args) {
+        int[][] arr = {
+                {1, 3, 5},
+                {1, 3, 7},
+                {2, 2, 3, 6}
+        };
+        Iterator iterator = new Iterator(arr);
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
     }
 }
+
